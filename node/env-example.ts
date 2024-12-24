f5e0e8fd03d34bc388a56aa4ec735c3f246d45cb
Eliton <eliton-jorge@hotmail.com>
2024-12-24 12:56:52 -0300

    feat: add environment variables to project

---
+++ node/env-example.ts
@@ -0,0 +1,9 @@
+//OIDC (Auth)
+export const OIDC_OP_TOKEN_ENDPOINT = ""
+export const OIDC_RP_CLIENT_SECRET = ""
+export const OIDC_RP_CLIENT_ID = ""
+
+// API Base URLs
+export const ENGAGE_API_BASE_URL = ""
+export const INTEGRATIONS_ENGINE_API_BASE_URL = ""
+export const COMMERCE_API_BASE_URL = ""
