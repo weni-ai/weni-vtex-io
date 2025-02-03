ab9e493ddcd1dec1804b24045f4e11a9b7c8b60e
Eliton Jorge <eliton-jorge@hotmail.com>
2025-02-03 11:05:11 -0300

    feat: add integrate feature middleware (#15)

--- node/index.ts
+++ node/index.ts
@@ -10,6 +10,7 @@ import { createAgentBuilder } from './middlewares/createAgentBuilder' // Middlew
 import { getOrdersByEmail } from './middlewares/getOrdersByEmail' // Middleware to fetch orders by user email
 import { getOrderFormDetails } from './middlewares/getOrderFormDetails' // Middleware to fetch order form details by ID
 import { validateInternalUserAuth } from './middlewares/validateInternalUserAuth' // Middleware to validate internal user requests
+import { integrateFeature } from './middlewares/integrateFeature' // Middleware to integrate features
 
 
 // Setting cache duration in milliseconds and creating an LRUCache instance
@@ -71,6 +72,10 @@ export default new Service({
     }),
     getOrderFormDetails: method({
       GET: [validateInternalUserAuth, getOrderFormDetails],
+    }),
+
+    integrateFeature: method({
+      POST: [validateInternalUserAuth, integrateFeature],
     }),
 
   },
